3b891437f3c0070d9018b3c86bf5cc1fccf946f1
pavithra <pavithrakanth14@gmail.com>
2022-12-04 20:26:13 +0530

    [Pavithra]-PresentAbsent

---
+++ Employeewage/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Employeewage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
